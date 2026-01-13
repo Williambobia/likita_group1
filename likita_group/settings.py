61ab5d602f77b2f305fa70ab4949511744d694bd
LIKITA Group <contact@likitagroup.com>
2026-01-14 00:32:19 +0100

    Add Procfile for Render

--- likita_group/settings.py
+++ likita_group/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 # ============================
 # MEDIA FILES
